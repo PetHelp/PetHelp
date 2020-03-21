6bcc0c60a1156be95a98c6a6bae26977ce525351
Sebastian Pfliegel <pfliegel.sebastian@gmail.com>
2020-03-21 20:17:10 +0100

    Axios + vuex + docker fix (history mode)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -11,5 +12,6 @@ Vue.use(IconsPlugin)
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
