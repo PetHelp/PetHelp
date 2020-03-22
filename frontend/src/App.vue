f193f71d3b4aea208b309a2b6cfe3b037c2b2084
Søren Birkemeyer <sb@webfactory.de>
2020-03-22 02:17:33 +0100

    Stop centering everything, just center navigation items for now

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -31,12 +31,12 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
 #nav {
   padding: 30px;
+  text-align: center;
 }
 
 #nav a {
