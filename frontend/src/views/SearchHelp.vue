a82bc25aee2726a99d9d4f1de5cc0df5e3955096
Søren Birkemeyer <sb@webfactory.de>
2020-03-22 17:24:42 +0100

    Add search mockup

--- frontend/src/views/SearchHelp.vue
+++ frontend/src/views/SearchHelp.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h1>Finde Hilfe</h1>
+  <div class="page col-12">
+    <div class="container">
+    <b-row>
+      <b-col cols="12">
+        <h1>Finde Hilfe</h1>
+      </b-col>
+    </b-row>
+    </div>
   </div>
 </template>
