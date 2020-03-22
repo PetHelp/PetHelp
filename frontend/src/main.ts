f40f8b76ceebb63d8662f51cf0309c9334eaeb77
Sebastian Pfliegel <pfliegel.sebastian@gmail.com>
2020-03-22 21:00:31 +0100

    More state handling

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,13 +6,13 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import VueFlashMessage from 'vue-flash-message'
+// import VueFlashMessage from 'vue-flash-message'
 import 'bootstrap-vue/dist/bootstrap-vue.js'
 require('vue-flash-message/dist/vue-flash-message.min.css')
 
 Vue.config.productionTip = false
 
-Vue.use(VueFlashMessage)
+// Vue.use(VueFlashMessage)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
