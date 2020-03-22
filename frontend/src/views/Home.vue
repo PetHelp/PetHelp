e2f34be80fd3ea02a1984eb7be6d772be833b05f
Kevin Müller <eichimueller@web.de>
2020-03-22 10:39:27 +0100

    New frontend/src/views/Home.vue und Bilder hinzugefügt

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="Page">
     <div class="container my-5 px-0 z-depth-1">
 
   <!--Section: Content-->
@@ -10,15 +10,15 @@
     <div class="row d-flex justify-content-center">
 
       <!--Grid column-->
-      <div class="col-md-6">
+      <div class="col-md-10">
 
-          <h3 class="font-weight-bold">Call to action</h3>
+          <h3 class="font-weight-bold">Bereits 20.189 gefundene Pfleegestellen!</h3>
 
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id quam sapiente molestiae
-            numquam quas, voluptates omnis nulla ea odio quia similique corrupti magnam.</p>
+          <p>Hast du ein Tier, dass schnellst möglich durch Eintritt einer Krankheit oder Quarantäne in gute Hände zur Pflege gegeben werden muss oder du brauchst jemand der sich
+            stationär um deine Tiere kümmert? Dann bist du bei UNS genau richtig! </p>
 
-          <a href="#" class="btn btn-outline-white btn-md waves-effect ml-0" role="button"><i class="fas fa-clone mr-2"></i>View project</a>
-          <a href="#" class="btn btn-outline-white btn-md waves-effect" role="button"><i class="fas fa-download mr-2"></i>Download</a>
+          <button type="button" class="btn btn-secondary" style="margin: 0px 100px 0px 0px">Ich suche Hilfe</button>
+          <button type="button" class="btn btn-secondary">Ich biete Hilfe</button>
 
       </div>
       <!--Grid column-->
@@ -30,28 +30,46 @@
   <!--Section: Content-->
 
 </div>
-    <p>Bereits 20.189 gefundene Pfleegestellen!</p>
-    <div class="container pt-5 my-5 z-depth-1">
+    <div class="container pt-5 my-5 z-depth-1 ">
   <section class="p-md-3 mx-md-5 text-center">
-    <h2 class="text-center mx-auto font-weight-bold mb-4 pb-2">Our Team</h2>
-    <div class="row">
+    <h2 class="text-center mx-auto font-weight-bold mb-4 pb-2">7.651 Tiere brauchen deine Hilfe</h2>
+    <div class="row Bereich">
       <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
         <div class="p-4">
           <div
             class="avatar w-100 white d-flex justify-content-center align-items-center"
           >
             <img
-              src="https://mdbootstrap.com/img/Photos/Avatars/img%20(9).jpg"
+              src="../assets/Images/Hund1.png"
               class="img-fluid rounded-circle z-depth-1"
             />
           </div>
           <div class="text-center mt-3">
-            <h6 class="font-weight-bold pt-2">Alan Turing</h6>
+            <h6 class="font-weight-bold pt-2">Steve</h6>
+            <p class="text-muted">
+              <small>
+                <i>3 Jahre, sehr unkompliziert.</i>
+                </small>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+        <div class="p-4">
+          <div
+            class="avatar w-100 white d-flex justify-content-center align-items-center"
+          >
+            <img
+              src="../assets/Images/Hund2.png"
+              class="img-fluid rounded-circle z-depth-1"
+            />
+          </div>
+          <div class="text-center mt-3">
+            <h6 class="font-weight-bold pt-2">Tom</h6>
             <p class="text-muted">
               <small
                 ><i
-                  >Lorem ipsum dolor sit amet, consectetur adipiscing
-                  elit.</i
+                  >2 Jahre, Vegan, liebt Spielzeug</i
                 ></small
               >
             </p>
@@ -64,30 +82,7 @@
             class="avatar w-100 white d-flex justify-content-center align-items-center"
           >
             <img
-              src="https://mdbootstrap.com/img/Photos/Avatars/img%20(32).jpg"
-              class="img-fluid rounded-circle z-depth-1"
-            />
-          </div>
-          <div class="text-center mt-3">
-            <h6 class="font-weight-bold pt-2">Tom Johnson</h6>
-            <p class="text-muted">
-              <small
-                ><i
-                  >Lorem ipsum dolor sit amet, consectetur adipiscing
-                  elit.</i
-                ></small
-              >
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
-        <div class="p-4">
-          <div
-            class="avatar w-100 white d-flex justify-content-center align-items-center"
-          >
-            <img
-              src="https://mdbootstrap.com/img/Photos/Avatars/img%20(3).jpg"
+              src="../assets/Images/Hund3.jpg"
               class="img-fluid rounded-circle z-depth-1"
             />
           </div>
@@ -96,8 +91,7 @@
             <p class="text-muted">
               <small
                 ><i
-                  >Lorem ipsum dolor sit amet, consectetur adipiscing
-                  elit.</i
+                  >5 und 4 Jahre, Nur zusammen abzugeben.</i
                 ></small
               >
             </p>
@@ -110,17 +104,16 @@
             class="avatar w-100 white d-flex justify-content-center align-items-center"
           >
             <img
-              src="https://mdbootstrap.com/img/Photos/Avatars/img%20(2).jpg"
+              src="../assets/Images/Hund4.jpg"
               class="img-fluid rounded-circle z-depth-1"
             />
           </div>
           <div class="text-center mt-3">
-            <h6 class="font-weight-bold pt-2">Nathalie Fry</h6>
+            <h6 class="font-weight-bold pt-2">Nathalie</h6>
             <p class="text-muted">
               <small
                 ><i
-                  >Lorem ipsum dolor sit amet, consectetur adipiscing
-                  elit.</i
+                  >1 Jahr, braucht besondere Betreuung.</i
                 ></small
               >
             </p>
@@ -133,17 +126,16 @@
             class="avatar w-100 white d-flex justify-content-center align-items-center"
           >
             <img
-              src="https://mdbootstrap.com/img/Photos/Avatars/img%20(31).jpg"
+              src="../assets/Images/Hund5.jpg"
               class="img-fluid rounded-circle z-depth-1"
             />
           </div>
           <div class="text-center mt-3">
-            <h6 class="font-weight-bold pt-2">Emma Lovelace</h6>
+            <h6 class="font-weight-bold pt-2">Emi</h6>
             <p class="text-muted">
               <small
                 ><i
-                  >Lorem ipsum dolor sit amet, consectetur adipiscing
-                  elit.</i
+                  >4 Jahre, sehr aktiv</i
                 ></small
               >
             </p>
@@ -156,17 +148,16 @@
             class="avatar w-100 white d-flex justify-content-center align-items-center"
           >
             <img
-              src="https://mdbootstrap.com/img/Photos/Avatars/img%20(5).jpg"
+              src="../assets/Images/Hund6.jpg"
               class="img-fluid rounded-circle z-depth-1"
             />
           </div>
           <div class="text-center mt-3">
-            <h6 class="font-weight-bold pt-2">Joanna Thompson</h6>
+            <h6 class="font-weight-bold pt-2">Lisi</h6>
             <p class="text-muted">
               <small
                 ><i
-                  >Lorem ipsum dolor sit amet, consectetur adipiscing
-                  elit.</i
+                  >3 Jahre, sehr ruhig</i
                 ></small
               >
             </p>
@@ -179,17 +170,16 @@
             class="avatar w-100 white d-flex justify-content-center align-items-center"
           >
             <img
-              src="https://mdbootstrap.com/img/Photos/Avatars/img%20(8).jpg"
+              src="../assets/Images/Hund7.jpg"
               class="img-fluid rounded-circle z-depth-1"
             />
           </div>
           <div class="text-center mt-3">
-            <h6 class="font-weight-bold pt-2">Billy Turner</h6>
+            <h6 class="font-weight-bold pt-2">Billy</h6>
             <p class="text-muted">
               <small
                 ><i
-                  >Lorem ipsum dolor sit amet, consectetur adipiscing
-                  elit.</i
+                  >4 Jahre, sehr laut</i
                 ></small
               >
             </p>
@@ -202,34 +192,32 @@
             class="avatar w-100 white d-flex justify-content-center align-items-center"
           >
             <img
-              src="https://mdbootstrap.com/img/Photos/Avatars/img%20(4).jpg"
+              src="../assets/Images/Hund8.jpg"
               class="img-fluid rounded-circle z-depth-1"
             />
           </div>
           <div class="text-center mt-3">
-            <h6 class="font-weight-bold pt-2">Veronica Smith</h6>
+            <h6 class="font-weight-bold pt-2">Sinchen</h6>
             <p class="text-muted">
               <small
                 ><i
-                  >Lorem ipsum dolor sit amet, consectetur adipiscing
-                  elit.</i
+                  >7 Jahre, absoluter Familienhund</i
                 ></small
               >
             </p>
           </div>
         </div>
       </div>
+      <button type="button" class="btn btn-primary">Mehr anzeigen...</button>
     </div>
   </section>
 </div>
 
- <button type="button" class="btn btn-primary">Mehr anzeigen...</button>
-
 <div class="container pt-5 my-5 z-depth-1">
   <section class="p-md-3 mx-md-5 text-center text-lg-left">
-    <h2 class="text-center mx-auto font-weight-bold mb-4 pb-2">Our Team</h2>
+    <h2 class="text-center mx-auto font-weight-bold mb-4 pb-2">Wir helfen Tierbesitzer*innen in Quarantäne</h2>
     <div class="row">
-      <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="col-lg-3 col-md-4 col-sm-6 mb-4  Bereich">
         <div class="p-4">
           <div
             class="avatar w-100 white d-flex justify-content-center align-items-center"
@@ -244,16 +232,16 @@
             <p class="text-muted">
               <small
                 ><i
-                  >Lorem ipsum dolor sit amet, consectetur adipiscing
-                  elit.</i
+                  >27 Jahre, Student</i
                 ></small
               >
             </p>
+            <button type="button" class="btn btn-primary ContactButton">Helfer*in kontaktieren</button>
           </div>
         </div>
       </div>
 
-      <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="col-lg-3 col-md-4 col-sm-6 mb-4  Bereich">
         <div class="p-4">
           <div
             class="avatar w-100 white d-flex justify-content-center align-items-center"
@@ -268,16 +256,16 @@
             <p class="text-muted">
               <small
                 ><i
-                  >Lorem ipsum dolor sit amet, consectetur adipiscing
-                  elit.</i
+                  >29 Jahre, Bloggerin</i
                 ></small
               >
             </p>
+            <button type="button" class="btn btn-primary ContactButton">Helfer*in kontaktieren</button>
           </div>
         </div>
       </div>
 
-      <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="col-lg-3 col-md-4 col-sm-6 mb-4  Bereich">
         <div class="p-4">
           <div
             class="avatar w-100 white d-flex justify-content-center align-items-center"
@@ -292,15 +280,15 @@
             <p class="text-muted">
               <small
                 ><i
-                  >Lorem ipsum dolor sit amet, consectetur adipiscing
-                  elit.</i
+                  >32 Jahre, Jurist</i
                 ></small
               >
             </p>
+            <button type="button" class="btn btn-primary ContactButton">Helfer*in kontaktieren</button>
           </div>
         </div>
       </div>
-      <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="col-lg-3 col-md-4 col-sm-6 mb-4  Bereich">
         <div class="p-4">
           <div
             class="avatar w-100 white d-flex justify-content-center align-items-center"
@@ -315,11 +303,11 @@
             <p class="text-muted">
               <small
                 ><i
-                  >Lorem ipsum dolor sit amet, consectetur adipiscing
-                  elit.</i
+                  >22 Jahre, Studentin</i
                 ></small
               >
             </p>
+            <button type="button" class="btn btn-primary ContactButton">Helfer*in kontaktieren</button>
           </div>
         </div>
       </div>
@@ -337,3 +325,21 @@ export default {
   name: 'Home'
 }
 </script>
+
+<style scoped>
+  .Bereich {
+border-color: gray;
+border-width: 2px;
+border-style: inset;
+border-radius: 10px;
+background-color:whitesmoke;
+            }
+
+  .ContactButton{
+    font-size: 12px
+      }
+
+  .Page{
+      background-color: #e3e3e3;
+        }
+</style>
