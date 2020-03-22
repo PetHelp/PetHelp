09f4aa1c66e3c327f1f6e847af309de3595961d6
Kevin Müller <eichimueller@web.de>
2020-03-22 17:03:11 +0100

    FAQ geändert und Guidlines integriert

--- frontend/src/views/Faq.vue
+++ frontend/src/views/Faq.vue
@@ -5,22 +5,28 @@
     <div class="card">
       <div class="card-header" id="headingOne">
         <h2 class="mb-0">
-          <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            Collapsible Group Item #1
+          <button class="btn btn-link QuestionBox" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            Wie finde ich eine geeignete Stelle zur Betreuung meines Tieres?
           </button>
         </h2>
       </div>
       <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
         <div class="card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          <b>Allgemeines</b><br>
+          Zunächst musst du dich auf unserer Platform registrieren und deinen Tier in deinem Profil mit seinen Daten registrieren. Dabei ist wichtig, dass du alle Eigenheiten des Tieres mit angibst.
+          <br><b>Stelle finden</b><br>
+          Anschließend hast du die Möglichkeit, die dir zu Verfügung stehenden Betreuer*innen unter "Hilfe suchen" anzuschauen. Dabei kannst du gezielt nach Menschen suchen, die zu deinem Tier passen.
+          Ebenfalls hast du die Möglichkeit, auch nach Entfernung zu der Pflegestelle bzw. der betreuenden Person zu filtern.
+          <br><b>Kontak aufnehmen</b><br>
+          Hast du eine Stelle gefunden, welche zu dir und deinem Tier passt, hast du die Möglichkeit die Person direkt über unsere Seite zu kontaktieren.
         </div>
       </div>
     </div>
 
     <div class="card">
       <div class="card-header" id="headingTwo">
-        <h2 class="mb-0">
-          <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        <h2 class="mb-0 ">
+          <button class="btn btn-link collapsed QuestionBox" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
             Collapsible Group Item #2
           </button>
         </h2>
@@ -35,7 +41,7 @@
     <div class="card">
       <div class="card-header" id="headingThree">
         <h2 class="mb-0">
-          <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          <button class="btn btn-link collapsed QuestionBox" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
             Collapsible Group Item #3
           </button>
         </h2>
@@ -46,7 +52,23 @@
         </div>
       </div>
     </div>
-  </div>
+
+  <div class="card">
+        <div class="card-header" id="headingFour">
+          <h2 class="mb-0">
+            <button class="btn btn-link collapsed QuestionBox" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+              Collapsible Group Item #4
+            </button>
+          </h2>
+        </div>
+        <div id="collapseFour" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+          <div class="card-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          </div>
+        </div>
+      </div>
+    </div>
+
 </div>
 </template>
 
@@ -54,5 +76,10 @@
   .wrapper{
     width: 1280px;
     margin: 0 auto;
+  }
+
+  .QuestionBox{
+    background: #2c4e57;
+    color: white;
   }
 </style>
--- frontend/src/views/Guideline.vue
+++ frontend/src/views/Guideline.vue
@@ -1,5 +1,116 @@
 <template>
-  <div class="about">
-    <h1>Guideline</h1>
+  <div class="wrapper">
+    <div class="wrapper">
+      <div class="mb-3" style=" border-style: solid; border-width: 1px; border-color: #2c4e57;border-radius: 5px; padding: 1em;">
+        <b style="font-size: 25px; text-decoration: underline black">Das Wichtigste zuerst:</b>
+        <br/>Nach aktuellem Stand kann das Coronavirus <b>nicht</b> von Haustieren auf Menschen oder umgekehrt von Menschen auf Haustieren übertragen werden!
+        <a
+          href="https://www.bmel.de/DE/Ministerium/_Texte/corona-virus-faq-fragen-antworten.html#doc13796248bodyText6"
+        >(Quelle zum BMEL)</a>
+        <br />Diese Plattform und alle hier angebotenen Hilfen sind rein ehrenamtlich,
+        <b>kommerzielle Angebote sind verboten!</b>
+      </div>
+    </div>
+
+    <div class="accordion" id="accordionExample">
+      <div class="card">
+        <div class="card-header" id="headingOne">
+          <h2 class="mb-0">
+            <button
+              class="btn btn-link QuestionBox"
+              type="button"
+              data-toggle="collapse"
+              data-target="#collapseOne"
+              aria-expanded="true"
+              aria-controls="collapseOne"
+            >Was ist an hygienischen Maßnahmen zu beachten?</button>
+          </h2>
+        </div>
+        <div
+          id="collapseOne"
+          class="collapse show"
+          aria-labelledby="headingOne"
+          data-parent="#accordionExample"
+        >
+          <div class="card-body">
+            <ul>- Bestätigt infizierte Personen sollten den engen Kontakt zu ihren Haustieren, wie z. B. das Abschlecken des Gesichts durch die Tiere, vermeiden.</ul>
+            <ul>- Im Idealfall sollte der direkte Kontakt zwischen Hilfe suchendem und helfenden vermieden werden. Die Übergabe von beispielsweise Hunden kann beispielsweise so erfolgen,
+            dass der Hund aus dem Flur abgeholt wird (während der Halter sich in einem anderen Raum aufhält) oder der Hund im Treppenhaus angebunden und dann abgeholt wird. Außerdem gilt auch hier,
+            Händewaschen nach Kontakt mit dem Tier, ggf. eigene Ausrüstung (beispielsweise Leine) mitbringen.</ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-header" id="headingTwo">
+          <h2 class="mb-0">
+            <button
+              class="btn btn-link collapsed QuestionBox"
+              type="button"
+              data-toggle="collapse"
+              data-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+            >Was ist bei der Pflege fremder Tiere zu beachten?</button>
+          </h2>
+        </div>
+        <div
+          id="collapseTwo"
+          class="collapse"
+          aria-labelledby="headingTwo"
+          data-parent="#accordionExample"
+        >
+          <div class="card-body">
+            <ul>- Es sollte im Vorfeld über mögliche Allergien und Krankheiten gesprochen werden. Ggf. muss über notwendige Medikamente gesprochen werden.</ul>
+            <ul>- Bitte klären Sie im Vorfeld ab, was passiert wenn das Tier sich verletzt oder krank wird. Soll die Pflegestelle im Zweifelsfall direkt zum Tierarzt fahren und
+            vom Tierarzt empfohlene Untersuchungen durchführen lassen? Oder regelt der Tierhalter in dem Fall alles Nötige?</ul>
+            <ul>- Ein Futterwechsel sollte möglichst vermieden werden.</ul>
+            <ul>- Besondere Eigenheiten des Tieres sollten geklärt werden. (Macht Dinge kaputt, Flieht gerne aus der Wohnung, mag keine Menschen mit Bart o.ä.)</ul>
+            <ul>- Für den Notfall sollte mindestens eine weitere Kontaktperson angegeben werden. (Plötzlicher Krankenhausaufenthalt o.ä.)</ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-header" id="headingThree">
+          <h2 class="mb-0">
+            <button
+              class="btn btn-link collapsed QuestionBox"
+              type="button"
+              data-toggle="collapse"
+              data-target="#collapseThree"
+              aria-expanded="false"
+              aria-controls="collapseThree"
+            >Was ist bei Spaziergängen mit fremden Hunden zu beachten?</button>
+          </h2>
+        </div>
+        <div
+          id="collapseThree"
+          class="collapse"
+          aria-labelledby="headingThree"
+          data-parent="#accordionExample"
+        >
+          <div class="card-body">
+            <ul>- Die Hunde sollten nicht von der Leine gelassen werden, hier können Schleppleinen Abhilfe schaffen.</ul>
+            <ul>- Kontakt mit anderen Hunden sollte wenn überhaupt nach Abfrage mit beiden Tierhaltern erfolgen (insbesondere dem Hilfesuchenden!).</ul>
+            <ul>- Halten Sie sich an Absprachen! (Maulkorb o.ä.)</ul>
+            <ul>- Zum Ausführen von Listenhunden müssen die Gassigeher in einigen Bundesländern Auflagen erfüllen. Ebenso gilt in einigen Bundesländern die 2040 Regel!</ul>
+            <ul>- Für jeden Hund muss eine gültige Haftpflichtversicherung abgeschlossen sein, und der Hund sollte geimpft sein.</ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.wrapper {
+  width: 1280px;
+  margin: 0 auto;
+}
+
+.QuestionBox {
+  background: #2c4e57;
+  color: white;
+}
+</style>
