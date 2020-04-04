7287406a512ed871520223750aa6dd4f48ae986e
Sebastian Pfliegel <pfliegel.sebastian@gmail.com>
2020-04-04 16:13:21 +0200

    Mixin to change document title

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,12 +9,15 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 // import VueFlashMessage from 'vue-flash-message'
 import 'bootstrap-vue/dist/bootstrap-vue.js'
 // require('vue-flash-message/dist/vue-flash-message.min.css')
+import titleMixin from './mixins/title'
 
 Vue.config.productionTip = false
 
 // Vue.use(VueFlashMessage)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+
+Vue.mixin(titleMixin)
 
 new Vue({
   router,
